9dd538ad0484389f62b4eceb4235eeb7ba0bdec7
THEO-184 <theophilusboakye47@gmail.com>
2022-02-18 15:48:11 +0000

    redux store setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,16 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import Something from "../features/counter/Something";
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+	reducer: {
+		counter: Something,
+	},
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
 >;
---
+++ src/features/counter/Something.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const Something = createSlice({
+	name: "somethinh=g",
+	initialState,
+	reducers: {},
+});
+
+export const {} = Something.actions;
+
+export default Something.reducer;
